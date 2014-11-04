89231af9c201fb519ef94bb44a6f65b3d95795b6
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-03 16:47:36 -0800

    adding test for four of a kind

--- spec.rb
+++ spec.rb
@@ -68,6 +68,11 @@ describe Hand do
       hand = create_hand(['9', '9', 'Q', 'Q', 'K'])
       expect(hand.rank).to eq('two pair')
     end
+
+    it "returns four of a kind" do
+      hand = create_hand(['9', '9', '9', '9', 'K'])
+      expect(hand.rank).to eq('four of a kind')
+    end
   end
 
 end
